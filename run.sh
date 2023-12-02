44db2edf11896a6ac027ed0acc923654cf7f393d
tropping <troppingz@gmail.com>
2023-12-02 16:36:14 -0600

    fix index oob.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for i in $(seq 1 10); do
-	python generate.py --outdir=cifar-10-$i --seeds=0-50 --class=$i \
+for i in $(seq 0 9); do
+	python generate.py --outdir=cifar-10-$i --seeds=0-2000 --class=$i \
 		    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
 done

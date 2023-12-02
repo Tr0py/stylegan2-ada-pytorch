e70ce9b181ea6f5d04b351e05ba60df8a28578fb
tropping <troppingz@gmail.com>
2023-12-02 16:57:30 -0600

    Save outputs.

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
+echo "installing requirements"
+pip install -r ./requirements.txt
+
 for i in $(seq 0 9); do
 	echo "generating images for Class $i"
 	python generate.py --outdir=cifar-10-$i --seeds=0-2000 --class=$i \
 		    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
+	echo "output is saved in cifar-10-$i"
 done
+
+tar -czvf cifar-10.tar.gz cifar-10-*
+echo "output is saved in cifar-10.tar.gz"

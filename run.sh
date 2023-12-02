369204746e5e2db7ae27e775d6cb41a797cf3360
tropping <troppingz@gmail.com>
2023-12-02 16:38:41 -0600

    Print progress.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 for i in $(seq 0 9); do
+	echo "generating images for Class $i"
 	python generate.py --outdir=cifar-10-$i --seeds=0-2000 --class=$i \
 		    --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
 done
